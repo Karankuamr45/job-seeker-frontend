0e94a1f0990d91050c4c05970b72265a89e341c0
karan <karankumar46226@gmail.com>
2024-02-20 17:09:31 +0530

    api url updated

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   useEffect(()=>{
     const fetchUser = async()=>{
       try {
-        const response=await axios.get("http://localhost:4000/api/user/getUser",{withCredentials:true});
+        const response=await axios.get("https://job-seeker-l2sv.onrender.com/api/user/getUser",{withCredentials:true});
         setUser(response.data.user);
         setIsAuthorized(true)
       } catch (error) {
